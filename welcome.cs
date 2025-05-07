af2857d22d66e3daf89d19f7626e3bc5becb5a2b
Adarshpatel07 <adarshpatel9329@gmail.com>
2025-05-07 14:14:45 +0530

    Welcome Program

---
+++ welcome.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WelcomeAdarsh
+{
+    public class Welcome
+    {
+        public static void Main(string[] args){
+        Console.WriteLine("Welcome Adarsh In This World Of C Sharp");
+        }
+    }
+}
