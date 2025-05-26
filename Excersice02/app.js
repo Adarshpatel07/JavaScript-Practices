1f63550ac7318428f9b4ccc06bc0a4383f2db386
Adarshpatel07 <adarshpatel9329@gmail.com>
2025-05-27 00:07:48 +0530

    second commit

--- Excersice02/app.js
+++ Excersice02/app.js
@@ -3,16 +3,18 @@ let ul = document.querySelector('ul');
 let li = document.querySelector('li');
 
 div.addEventListener('click', function(event) {
+  event.stopPropagation();
   console.log('Div was clicked');
 })
 
 ul.addEventListener('click', function(event){
+        event.stopPropagation();
     console.log('Ul was clicked');
 })
 
-
-for(let li of lis){
-    li.addEventListner('click', function(event){
+for(let lis of li){
+    lis.addEventListner('click', function(event){
+    event.stopPropagation();
     console.log('Li was clicked');
     })
 }
